f9313f8e84df1301d8c8afae230544d0fade04b4
TAREASDEINFROMATICA <ronaldocharca3@gmail.com>
2024-12-22 12:31:50 -0400

    Add files via upload

---
+++ proyectofinal/src/pages/CATALOGO.js
@@ -0,0 +1,97 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Componente2 } from './components/Componente2';
+import { Componente1 } from './components/Componente1';
+import { Componente3 } from './components/Componente3';
+import { Componente4 } from './components/Componente4';
+import './CATALOGO.css';
+function CATALOGO () {
+    
+    return(
+        <div className="Padre">
+            <div className="Padre2">
+            <div className="Madre">
+            <div className="Madre2">  
+            
+            <div className="Hijo">
+            <div><h1> <Componente1 Texto="Torta Tres Leches"></Componente1> </h1> <Componente2 img="Torta1.jpg"></Componente2>
+                        <h1><Componente4 Precio="200Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta De Choco Late"></Componente1> </h1><Componente2 img="Torta2.png"></Componente2>
+                        <h1><Componente4 Precio="80Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Quequitos"></Componente1></h1><Componente2 img="Torta3.png"></Componente2>
+                        <h1><Componente4 Precio="111Bs"></Componente4></h1></div>
+                        <div> <h1><Componente1 Texto="Torta Quequitos"></Componente1></h1><Componente2 img="Torta3.png"></Componente2>
+                        <h1><Componente4 Precio="111Bs"></Componente4></h1></div>
+            
+            </div>
+            <div className="Pie">
+            <Componente3 pregunta="INGREDIENTES "   historia="Estos ingredientes básicos para hornear un pastel son; harina, huevos, grasa (generalmente mantequilla), azúcar, sal, una forma de líquido (generalmente leche) y agentes leudantes (como bicarbonato de sodio) lodemas es secreto profecional ." ></Componente3>
+            </div>
+            <div className='Hijo'>
+            <div><h1> <Componente1 Texto="Torta Especial"></Componente1> </h1> <Componente2 img="Torta4.jpg"></Componente2>
+                        <h1><Componente4 Precio="300Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="TORTAS ESPECIAL"></Componente1> </h1><Componente2 img="Torta4.png"></Componente2>
+                        <h1><Componente4 Precio="320Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="TORTAS ESPECIAL"></Componente1></h1><Componente2 img="Torta6.jpg"></Componente2>
+                        <h1><Componente4 Precio="299Bs"></Componente4></h1></div>
+                        <div> <h1><Componente1 Texto="TORTAS ESPECIAL"></Componente1></h1><Componente2 img="Torta4.jpg"></Componente2>
+                        <h1><Componente4 Precio="299Bs"></Componente4></h1></div>
+            
+            </div>
+            <div className="Pie">
+            <Componente3 pregunta="INGREDIENTES" historia="Estos ingredientes básicos para hornear un pastel son; harina, huevos, grasa (generalmente mantequilla), azúcar, sal, una forma de líquido (generalmente leche) y agentes leudantes (como bicarbonato de sodio) lodemas es secreto profecional ."></Componente3>
+            </div>
+            <div className='Hijo'>
+            <div><h1> <Componente1 Texto="TOrta Cafe"></Componente1> </h1> <Componente2 img="Torta7.jpg"></Componente2>
+                        <h1><Componente4 Precio="150Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Orange Plus"></Componente1> </h1><Componente2 img="Torta8.jpg"></Componente2>
+                        <h1><Componente4 Precio="200Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Casata"></Componente1></h1><Componente2 img="Torta9.png"></Componente2>
+                        <h1><Componente4 Precio="210Bs"></Componente4></h1></div>
+                        <div> <h1><Componente1 Texto="Torta Casata"></Componente1></h1><Componente2 img="Torta3.png"></Componente2>
+                        <h1><Componente4 Precio="210Bs"></Componente4></h1></div>
+            
+            </div>
+            <div className="Pie">
+            <Componente3 pregunta="INGREDIENTES"  historia="Estos ingredientes básicos para hornear un pastel son; harina, huevos, grasa (generalmente mantequilla), azúcar, sal, una forma de líquido (generalmente leche) y agentes leudantes (como bicarbonato de sodio) lodemas es secreto profecional ."></Componente3>
+            </div>
+            <div className='Hijo'>
+            <div><h1> <Componente1 Texto="Torta Cumpleaños"></Componente1> </h1> <Componente2 img="Torta10.png"></Componente2>
+                        <h1><Componente4 Precio="100Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Coco"></Componente1> </h1><Componente2 img="Torta11.png"></Componente2>
+                        <h1><Componente4 Precio="100Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Simple"></Componente1></h1><Componente2 img="Torta12.png"></Componente2>
+                        <h1><Componente4 Precio="30Bs"></Componente4></h1></div>
+                        <div> <h1><Componente1 Texto="Torta Simple"></Componente1></h1><Componente2 img="Torta2.png"></Componente2>
+                        <h1><Componente4 Precio="30Bs"></Componente4></h1></div>
+            
+            </div>
+            <div className="Pie">
+            <Componente3 pregunta="INGREDINETES" historia="Estos ingredientes básicos para hornear un pastel son; harina, huevos, grasa (generalmente mantequilla), azúcar, sal, una forma de líquido (generalmente leche) y agentes leudantes (como bicarbonato de sodio) lodemas es secreto profecional ."></Componente3>
+            </div>
+            
+            <div className='Hijo'>
+            <div><h1> <Componente1 Texto="Torta Cromosa"></Componente1> </h1> <Componente2 img="Torta13.png"></Componente2>
+                        <h1><Componente4 Precio="100Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Blanca"></Componente1> </h1><Componente2 img="Torta14.png"></Componente2>
+                        <h1><Componente4 Precio="50Bs"></Componente4></h1></div>
+            <div> <h1><Componente1 Texto="Torta Orange"></Componente1></h1><Componente2 img="Torta15.png"></Componente2>
+                        <h1><Componente4 Precio="50Bs"></Componente4></h1></div>
+                        <div> <h1><Componente1 Texto="Torta Orange"></Componente1></h1><Componente2 img="Torta1.jpg"></Componente2>
+                        <h1><Componente4 Precio="50Bs"></Componente4></h1></div>
+            
+            </div>
+            <div className="Pie">
+            <Componente3 pregunta="INGREDINTES" historia="Estos ingredientes básicos para hornear un pastel son; harina, huevos, grasa (generalmente mantequilla), azúcar, sal, una forma de líquido (generalmente leche) y agentes leudantes (como bicarbonato de sodio) lodemas es secreto profecional ."></Componente3>
+            </div>
+            
+            </div>  
+
+            </div>
+            
+            </div>
+        </div>
+        /*USAMOS RUTTT  CART CUESTOS S REACTRAOP*/ 
+    );
+}
+export default CATALOGO;
